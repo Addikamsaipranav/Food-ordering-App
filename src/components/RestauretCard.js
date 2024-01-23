a58bc67da6bc9a8e9505845657159722cca4e88e
Addikamsaipranav <72406492+Addikamsaipranav@users.noreply.github.com>
2024-01-23 15:47:55 +0530

    Added Live Swiggy-API

--- src/components/RestauretCard.js
+++ src/components/RestauretCard.js
@@ -2,22 +2,28 @@ import { CARD_IMG_URL } from "../utils/const"
 const RestauretCard=(props)=>{
     const {resData}=props
     //lets optimize our app
-    const {cloudinaryImageId,name,cuisines,avgRating,costForTwo,deliveryTime}=resData?.data
+    console.log(resData)
+    // console.log(resData.info.id)
+    // console.log(resData.info.name)
+    // console.log(resData.info.cloudinaryImageId)
+    // console.log(resData.info.avgRating)
+    // console.log(resData.info.sla.deliveryTime)
+    
     return(
 
         <div className="res-card" >
     
-            <img className="res-logo" alt="res-log" src={CARD_IMG_URL+resData.data.cloudinaryImageId}></img>
+            <img className="res-logo" alt="res-log" src={CARD_IMG_URL+resData.info.cloudinaryImageId}></img>
             <div className="data"> 
             <h5 className="title" style={{
              }
              // here cusines are in form of array
              //we need to make it comma seperated
-             }>{name}</h5>
-             <span><h6 className="cuisines">{cuisines.join(", ")}</h6></span>
-             <h6>{avgRating}Stars</h6>
-             <h6>₹{costForTwo/100} For Two</h6>
-             <h6>{deliveryTime}</h6>
+             }>{resData.info.name}</h5>
+             <span><h6 className="cuisines">{resData.info.cuisines.join(", ")}</h6></span>
+             <h6>{resData.info.avgRating}Stars</h6>
+             <h6>₹{resData.info.costForTwo/100} For Two</h6>
+             <h6>{resData.info.sla.deliveryTime}</h6>
              </div>
         </div>
     )
